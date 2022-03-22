97041c4702e59d239fea3cafa9fcbf344be1ac9a
AJV11 <90346121+AJV11@users.noreply.github.com>
2022-03-21 21:33:30 -0400

    Update app.py

--- sample_site/app.py
+++ sample_site/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/result")
 def index():
 
     # Load current count
@@ -10,7 +11,7 @@ def index():
     count = int(f.read())
     f.close()
 
-    # Increment the count
+    # Increment the count 
     count += 1
 
     # Overwrite the count
@@ -18,12 +19,22 @@ def index():
     f.write(str(count))
     f.close()
 
-    # Render HTML with milestone variable 
-    if count == 50:
+    # Render HTML with milestone variables
+    if count % 1000 == 0:
+        return render_template("index.html", massivemilestone=count, count=count)
+
+    if count % 100 == 0:
+        return render_template("index.html", hugemilestone=count, count=count)
+
+    if count % 50 == 0:
+        return render_template("index.html", bigmilestone=count, count=count)
+
+    if count % 25 == 0:
         return render_template("index.html", milestone=count, count=count)
 
     # Render HTML with count variable
     return render_template("index.html", count=count)
+
 
 if __name__ == "__main__":
     app.run()
